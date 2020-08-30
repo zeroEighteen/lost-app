90b7ce14da22cc3eb11188d26e59772f1844141f
zeroEighteen <koh_yi_lei_elliot@s2019.ssts.edu.sg>
2020-08-30 14:30:30 +0800

    Add image uploading

--- src/pages/AddContact/index.tsx
+++ src/pages/AddContact/index.tsx
@@ -1,10 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import {ScrollView, Text, Platform, FlatList, Image} from 'react-native';
+import {
+  ScrollView,
+  Text,
+  Platform,
+  FlatList,
+  Image,
+  Button,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import {connect} from 'react-redux';
 import {Layout} from '@ui-kitten/components';
 import auth from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-community/google-signin';
 import Svg, {Circle, G, Path, Defs} from 'react-native-svg';
+import ImagePicker from 'react-native-image-crop-picker';
 
 import {K} from '../../store/constants';
 import {PageHeader} from '../../components/Page/PageHeader';
@@ -13,9 +22,20 @@ import {TextButton} from '../../components/Button/Text';
 import {ContentCard} from '../../components/Card/Content';
 import {LoginButton} from '../../components/Button/Login';
 import {SafeAreaView, View, Dimensions} from 'react-native';
-import {ImageCard} from './../../components/Card/Image';
 
 export const AddContactPage = (props: any) => {
+  const [imagePath, setImagePath] = useState(null);
+
+  const uploadImage = () => {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 300,
+      cropping: true,
+    }).then(image => {
+      setImagePath(image.path);
+    });
+  };
+
   return (
     <View>
       <PageHeader
@@ -25,6 +45,21 @@ export const AddContactPage = (props: any) => {
         navigation={props.navigation}
         leadingButton="arrow-left"
         onLeadingButtonPress={() => props.navigation.navigate('Contacts')}
+      />
+      {imagePath != null && (
+        <Image source={{uri: imagePath}} style={{height: 200, width: 200}} />
+      )}
+      {imagePath == null && (
+        <Image
+          source={require('./../../../assets/tests/blank.png')}
+          style={{height: 200, width: 200}}
+        />
+      )}
+      <TextButton
+        theme={props.theme}
+        children="Upload an Image"
+        style={{height: 50}}
+        onPress={uploadImage}
       />
     </View>
   );
